6972400c81486a1c0c6acd0ea60889503528e47f
lucianogaregnani <luciano.garegnani@estudiantes.unahur.edu.ar>
2024-01-26 17:04:51 -0300

    home mobile design

--- app/page.tsx
+++ app/page.tsx
@@ -6,11 +6,11 @@ export default function Home() {
         <span className="text-indigo-500 font-medium">prompts!</span>
       </div>
       <h1 className="textTitle text-indigo-gradient text-center">Discover & Share AI Prompts</h1>
-      <p className="text-center text-lg text-slate-600">
+      <p className="text-center text-md sm:text-lg text-slate-600">
         Promptopia is an open-source AI prompting tool for modern world to
         discover, create and share creative prompts
       </p>
-      <input type="text" placeholder="Search prompt..." className="w-full bg-slate-100 p-2 px-4 rounded-full max-w-xl border-2 border-slate-200 focus:border-indigo-400 focus:outline-none focus:ring-0"/>
+      <input type="text" placeholder="Search prompt..." className="w-[90%] sm:w-full bg-slate-100 p-2 px-4 rounded-full max-w-xl border-2 border-slate-200 focus:border-indigo-400 focus:outline-none focus:ring-0"/>
     </section>
   );
 }
--- components/Nav/SignedInSection.tsx
+++ components/Nav/SignedInSection.tsx
@@ -1,24 +1,30 @@
-"use client"
+"use client";
 
+import { IoIosAdd } from "react-icons/io";
 import { signOut } from "next-auth/react";
 import Image from "next/image";
 import Link from "next/link";
 
-function SignedInSection({ imageUrl }:{ imageUrl:string }) {
+function SignedInSection({ imageUrl }: { imageUrl: string }) {
+
   return (
-    <section className="flex items-center gap-3">
-      <button className="redBtn" onClick={() => signOut()}>
-        Sign out
-      </button>
-      <Link href="">
-        <Image
-          src={imageUrl}
-          alt="Profile picture"
-          width={45}
-          height={45}
-          className="rounded-full"
-        />
-      </Link>
+    <section className="flex gap-2">
+        <button className="fixed bottom-10 right-6 sm:transparent-normal-btn sm:static">
+          <span className="hidden sm:block">Create Prompt</span>
+          <span className="block sm:hidden text-5xl text-white bg-indigo-500 rounded-full"><IoIosAdd /></span>
+        </button>
+        <button className="redBtn" onClick={() => signOut()}>
+          Sign out
+        </button>
+        <Link href="">
+          <Image
+            src={imageUrl}
+            alt="Profile picture"
+            width={45}
+            height={45}
+            className="rounded-full"
+          />
+        </Link>
     </section>
   );
 }
