3640ba6693817dd3c260480761a088b9bb4c6e31
lucianogaregnani <luciano.garegnani@estudiantes.unahur.edu.ar>
2024-01-27 00:31:37 -0300

    modal design complete

--- components/Prompt/PromptModal.tsx
+++ components/Prompt/PromptModal.tsx
@@ -9,9 +9,10 @@ function PromptModal({ type }: { type: string }) {
 
   return (
     isOpen && (
-      <section className="z-30 fixed top-0 h-screen w-screen bg-black/40 transition-all flex flex-col justify-center items-center p-6">
+      <section className="z-30 fixed top-0 h-screen w-screen bg-black/40 flex flex-col justify-center items-center p-6">
+        <div onClick={closeModal} className="z-30 absolute h-screen w-screen" />
         <h1 className="text-white text-3xl font-semibold">{type} post</h1>
-        <form className="flex flex-col gap-4 py-5 px-9 sm:px-16 bg-slate-100 rounded-2xl items-center justify-center relative">
+        <form className="flex flex-col gap-4 py-5 px-9 sm:px-16 bg-slate-100 transition-all rounded-2xl items-center justify-center relative z-40">
           <button onClick={closeModal} className="absolute right-2 top-2 text-3xl text-slate-400 bg-slate-200 hover:bg-red-400 hover:text-red-200 rounded-full p-1 transition-all">
             <IoClose />
           </button>
