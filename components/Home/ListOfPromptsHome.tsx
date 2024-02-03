5f2ac83e61cfbe058d3e9dadcbe39958e90e3233
lucianogaregnani <luciano.garegnani@estudiantes.unahur.edu.ar>
2024-02-03 20:34:21 -0300

    fix

--- components/Home/ListOfPromptsHome.tsx
+++ components/Home/ListOfPromptsHome.tsx
@@ -3,9 +3,10 @@ import PromptsSkeleton from "@/Skeletons/PromptsSkeleton";
 import getPrompts from "@/services/getPrompts.service";
 import { Suspense } from "react";
 import { filterPrompts } from "@/utils/filterPrompt";
+import getPromptsByUserId from "@/services/getPromptsByUserId.service";
 
 async function ListOfPromptsHome({ query }: { query: string }) {
-  const prompts = await getPrompts();
+  const prompts = await getPromptsByUserId("65b3fecc1951fc76fe77ef15");
 
   const filteredPrompts = query ? filterPrompts(prompts, query) : prompts;
 
