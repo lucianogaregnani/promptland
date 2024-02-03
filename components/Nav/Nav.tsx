3ea15fd2646d7740e581ae6d2f24fca69e5b14f6
lucianogaregnani <luciano.garegnani@estudiantes.unahur.edu.ar>
2024-02-02 23:36:30 -0300

    logo

--- components/Nav/Nav.tsx
+++ components/Nav/Nav.tsx
@@ -4,14 +4,22 @@ import { useSession } from "next-auth/react";
 import SignInButton from "./SignInButton";
 import SignedInSection from "./SignedInSection";
 import Link from "next/link";
+import Image from "next/image";
+import logo from "@/assets/logo.png";
 
 function Nav() {
   const { data: session } = useSession();
 
   return (
     <nav className="fixed z-10 top-0 w-full flex justify-between p-4 items-center sm:backdrop-blur-[1px]">
-      <Link className="text-2xl font-semibold bg-white sm:bg-transparent sm:shadow-none shadow-sm p-3 rounded-full sm:bg-none sm:p-0" href="/">
-        prompt<span className="text-indigo-500">land</span>
+      <Link
+        className="flex gap-2 text-2xl font-semibold bg-white sm:bg-transparent sm:shadow-none shadow-sm p-3 rounded-full sm:bg-none sm:p-0"
+        href="/"
+      >
+        <Image height={35} width={35} alt="logo" src={logo} />
+        <p className="sm:block hidden">
+          prompt<span className="text-indigo-500">land</span>
+        </p>
       </Link>
       {session ? (
         <SignedInSection imageUrl={session.user?.image || ""} />
