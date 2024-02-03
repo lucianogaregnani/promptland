0a878785c4af87f7d30c7f3af7ca2530a4008d66
lucianogaregnani <luciano.garegnani@estudiantes.unahur.edu.ar>
2024-02-02 21:45:43 -0300

    fix

--- services/getUserById.service.ts
+++ services/getUserById.service.ts
@@ -1,7 +1,7 @@
 import { IUser } from "@/types/prompt.types";
 
 async function getUserById(userId:string):Promise<IUser> {
-    return fetch(`${process.env.URL}/api/user/${userId}`, { cache:"no-store" })
+    return fetch(`${process.env.URL}/api/user/${userId}`)
         .then(res => res.json());
 }
 
